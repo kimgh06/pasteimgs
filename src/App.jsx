50cf0cc38e6d963eacf2ff7f6e4a606f222f4743
kkh061101 <kkh061101@naver.com>
2023-07-11 08:06:11 +0900

    i just made it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [src, setSrc] = useState('');
+  useEffect(e => {
+    document.title = "copy the clipboard";
+  }, []);
   return <div>
-    hello
+    <input type="file" onPaste={e => {
+      const items = e.clipboardData.items[0];
+      if (items.type.indexOf('image') === 0) {
+        let blob = items.getAsFile();
+        let reader = new FileReader();
+        reader.onload = e => {
+          setSrc(e.target.result);
+        }
+        reader.readAsDataURL(blob);
+      }
+      console.log(items.type);
+    }} />
+    <img src={src} alt="pasteit" />
   </div>;
 }
