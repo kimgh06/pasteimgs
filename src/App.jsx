46d67662efb5767b127527b7557bbc4d43b3e53d
kkh061101 <kkh061101@naver.com>
2023-07-11 23:03:14 +0900

    added style.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import * as S from './style';
 
 export default function App() {
   const [src, setSrc] = useState('');
+  const typefileinput = useRef(null);
   useEffect(e => {
     document.title = "copy the clipboard";
   }, []);
-  return <div>
-    <input type="file" onPaste={e => {
-      const items = e.clipboardData.items[0];
-      if (items.type.indexOf('image') === 0) {
-        let blob = items.getAsFile();
+  return <S.App onPaste={e => { //붙여넣기 시
+    const items = e.clipboardData.items[0];
+    if (items.type.indexOf('image') === 0) {
+      let blob = items.getAsFile();
+      let reader = new FileReader();
+      reader.onload = e => {
+        setSrc(e.target.result);
+      }
+      reader.readAsDataURL(blob);
+    }
+    console.log(items.getAsFile());
+  }}>
+    <img src={src} alt="pasteit" />
+    paste it
+    <input ref={typefileinput} type="file" onChange={e => { //파일 끌어올 시
+      const input = typefileinput.current;
+      if (input.files[0]) {
         let reader = new FileReader();
         reader.onload = e => {
           setSrc(e.target.result);
         }
-        reader.readAsDataURL(blob);
+        console.log(input.files[0])
+        reader.readAsDataURL(input.files[0]);
       }
-      console.log(items.type);
-    }} />
-    <img src={src} alt="pasteit" />
-  </div>;
+    }}></input>
+  </S.App>;
 }
---
+++ src/style.js
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const App = styled.div`
+  input[type="file"]{ 
+    width: 75px;
+    cursor: pointer;
+    /* height: 1px;  */
+  }
+`
