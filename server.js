ca271809ba696f6bcf35cc078e277b8d09bf94c9
kkh061101 <kkh061101@naver.com>
2023-07-12 19:52:39 +0900

    토큰 발급기 제작

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require("cors");
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -8,11 +9,14 @@ const connection = mysql.createConnection({
 });
 connection.connect(err => {
   if (err) throw err;
-  console.log("database connected");
+  console.log("the database is connected");
 })
-
 const app = express();
 const port = 8888;
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, '0.0.0.0', e => {
   console.log(`this server is running on port ${port}`);
@@ -30,6 +34,47 @@ app.get('/chat/checkid', (rq, rs) => {
     connection.query(sql, (err, rst) => {
       if (err) throw err;
       rs.send(rst[0]?.id !== undefined);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.post('/chat/login', (rq, rs) => {
+  const body = rq.body;
+  let sql = `select * from users where nickname = '${body.nickname}' and pw = '${body.pw}'`; //해당 닉네임을 가지고 있는 유저의 정보를 불러옴.
+  try {
+    connection.query(sql, (err, rst, fld) => {
+      if (err) throw err;
+      if (rst[0]?.id !== undefined) {
+        const accessToken = Math.random().toString(36).substr(2, 11) + Math.random().toString(36).substr(2, 11);
+        const refreshToken = Math.random().toString(36).substr(2, 11) + Math.random().toString(36).substr(2, 11);
+        const time = new Date().getTime() / 1000 / 60; //분 단위
+        sql = `insert into tokens values(${rst[0].id}, '${accessToken}', '${refreshToken}', '${Math.floor(time)}')`;
+        connection.query(sql, (err, rst, fld) => {
+          if (err) throw err;
+          rs.send({
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            registTime: Math.floor(time) + 30
+          });
+        });
+      } else {
+        rs.send({ message: "not found" });
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.post('/chat/deltokens', (rq, rs) => {
+  const nickname = rq.body?.nickname;
+  let sql = `delete from tokens where id = (select id from users where nickname = '${nickname}')`;
+  try {
+    connection.query(sql, (err, rst, fld) => {
+      if (err) throw err;
+      rs.send({ affectedRows: rst.affectedRows });
     });
   } catch (e) {
     console.log(e);
--- src/chat/index.jsx
+++ src/chat/index.jsx
@@ -5,13 +5,15 @@ import axios from "axios";
 export default function Chat(e) {
   const [toput, setToput] = useState('');
   const [able, setAble] = useState(null);
+  const [logininfo, setLogininfo] = useState({ nickname: '', pw: '' });
+  const url = "http://localhost:8888";
   useEffect(e => {
     document.title = 'chat'
   }, []);
   return <S.Chat>
-    <form className="insertClientId" onSubmit={async e => {
+    <form className="checkId" onSubmit={async e => {
       e.preventDefault();
-      await axios.get(`http://localhost:8888/chat/checkid?id=${toput}`)
+      await axios.get(`${url}/chat/checkid?id=${toput}`)
         .then(e => {
           setAble(e.data);
         })
@@ -21,7 +23,27 @@ export default function Chat(e) {
     }}>
       <input onChange={e => setToput(e.target.value)} placeholder="확인할 id" />
       {able === null ? '' : able === true ? "사용할 수 없는 아이디 입니다." : "사용할 수 있는 아이디 입니다."}
-
+    </form>
+    <form className="login" onSubmit={async e => {
+      e.preventDefault();
+      await axios.post(`${url}/chat/deltokens`, { nickname: logininfo.nickname })
+        .then(async e => {
+          if (e.data.affectedRows > 0) {
+            await axios.post(`${url}/chat/login`, {
+              nickname: logininfo.nickname,
+              pw: logininfo.pw
+            }).then(e => {
+              console.log(e.data);
+            }).catch(e => {
+              console.log(e);
+            });
+          }
+        })
+        .catch(e => console.log(e));
+    }}>
+      <input onChange={e => setLogininfo(a => ({ ...a, nickname: e.target.value }))} placeholder="로그인 닉네임" />
+      <input onChange={e => setLogininfo(a => ({ ...a, pw: e.target.value }))} placeholder="로그인 비밀번호" />
+      <button>토큰 발급</button>
     </form>
   </S.Chat>
 }
